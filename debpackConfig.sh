c0e8296b41d1505e99765fdeb06a08718f19018f
Nilson R S Vieira <nilsonrsvieira@gmail.com>
2022-09-08 15:06:44 -0300

    Add VirtualBox 6.1 for Jammy

--- debpackConfig.sh
+++ debpackConfig.sh
@@ -56,6 +56,10 @@ function virtualboxDeb (){
     echo "---------------------------------"
     echo "Installing VirtualBox..."
     echo "---------------------------------"    
+
+    deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian jammy contrib
+    wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
+    sudo apt-get update && sudo apt-get install virtualbox-6.1
 }
 
 function zoomDeb (){
